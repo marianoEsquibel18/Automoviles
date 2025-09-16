785cbac421308c59f795b4c92ed88b39087f337e
MarianoEsquibel <mariano.esquibel@mi.unc.edu.ar>
2025-09-16 12:06:42 -0300

    commit2
    se elimino wheather forecast

---
+++ ApiAtumoviles/ApiAtumoviles/ApiAtumoviles/Migrations/20250915151100_InitialCreate.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiAutomoviles.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Automoviles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fabricacion = table.Column<int>(type: "int", nullable: false),
+                    NumeroMotor = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    NumeroChasis = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Automoviles", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Automoviles_NumeroChasis",
+                table: "Automoviles",
+                column: "NumeroChasis",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Automoviles_NumeroMotor",
+                table: "Automoviles",
+                column: "NumeroMotor",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Automoviles");
+        }
+    }
+}
---
+++ ApiAtumoviles/ApiAtumoviles/ApiAtumoviles/Models/Automovil.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiAutomoviles.Models
+{
+    public class Automovil
+    {
+        public int Id { get; set; } // autoincremental
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public int Fabricacion { get; set; }
+        public string NumeroMotor { get; set; } // único
+        public string NumeroChasis { get; set; } // único
+    }
+}
